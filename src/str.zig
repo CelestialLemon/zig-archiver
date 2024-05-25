687b4f41e54740c1d1c853578a9d74d9faae39a2
Ashutosh Shinde <ashutoshinde23@gmail.com>
2024-05-25 14:30:12 +0530

    strcat function implementation

--- src/str.zig
+++ src/str.zig
@@ -3,7 +3,12 @@ const std = @import("std");
 
 // alias
 const mem = std.mem;
+const page_allocator = std.heap.page_allocator;
 
 pub fn strcmp(a: []u8, b: []u8) bool {
     return mem.eql(u8, a, b);
+}
+
+pub fn strcat(segments: []const []u8) ![]u8 {
+    return mem.concat(page_allocator, u8, segments);
 }
